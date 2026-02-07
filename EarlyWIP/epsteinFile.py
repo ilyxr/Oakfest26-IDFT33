f72a7e165e04975cf7c402e31c958588bf7d77d0
mr beats <hackieuser@gmail.com>
2026-02-07 19:22:38 +0530

    Fixed background + formatting

--- EarlyWIP/epsteinFile.py
+++ EarlyWIP/epsteinFile.py
@@ -3,8 +3,9 @@ import pandas as pd
 import streamlit.components.v1 as components
 
 
-bg_url = "https://hitecher.com/storage/img/20190423/583ade3bcb4d91df0177.jpg"  
+bg_url = "/EarlyWIP/pages/bg.png"  
 css = f"""
+<link href="https://fonts.googleapis.com/css2?family=SUSE+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet">
 <style>
   .stApp {{
     background-image: url("{bg_url}");
@@ -15,9 +16,20 @@ css = f"""
   }}
   .bg-overlay {{
     position: fixed; inset: 0; pointer-events: none; z-index: 0;
-    background: rgba(0,5,75,0.25);
+    background: rgba(0,0,25,0);
   }}
   .main > div {{ position: relative; z-index: 1; }}
+  
+  .stTextInput input {{
+    background-color: rgba(0, 0, 0, 0) !important;
+    border: 0px solid rgba(0, 0, 0, 0) !important;
+    outline: 0px solid rgba(0, 0, 0, 0) !important;
+    padding: 12px 20px !important;
+    color: rgba(255, 255, 255, 0.9) !important;
+    text-align: center !important;
+    box-sizing: border-box !important;
+  }}
+
 </style>
 <div class="bg-overlay"></div>
 """
@@ -41,8 +53,7 @@ st.write(" ")
 st.write(" ") 
 st.write(" ") 
 
-
-st.markdown("<h1 style='text-align: center; color: white; font-family:monospace; padding: 67px'>t r i l u n a</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-size: 4rem; text-align: center; color: white; font-family: Tourney, monospace; padding: 67px'>t r i l u n a</h1>", unsafe_allow_html=True)
 
 github_link = st.text_input(
     " ",
@@ -54,7 +65,3 @@ if github_link:
     with st.spinner("going through your hard work..."):
         # put the actual backend here 
         st.switch_page("resultsPage.py")
-            
-        
-        
-
--- EarlyWIP/pages/resultsPage.py
+++ EarlyWIP/pages/resultsPage.py
@@ -1,20 +1,70 @@
-
 # results page 
 import streamlit as st 
 import pandas as pd 
 
-st.write("Here are the results of the analysis: ")
-col1, col2 = st.columns(2) 
+# padding? nah 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
+st.write(" ") 
 
-with col1: 
-    st.write(
-        pd.DataFrame(
-            {
-                "Problem": ["problem 1", "problem 2", "problem 3"],
-                "Description": ["explanation 1", "explanation 2", "explanation 3"],
-            }
-        ))
-    
-with col2: 
-    st.write("Overall Rating:")
-    st.progress(80, text="4/5")
+bg_url = "https://instagram.fblr20-2.fna.fbcdn.net/v/t1.15752-9/624576839_1959843261553319_717761765649431422_n.png?_nc_cat=104&_nc_cb=04374bf5-24e287ff&ccb=7-5&_nc_sid=fc17b8&efg=eyJxZV9ncm91cHMiOlsiaWdkX2Jlc3RfZWZmb3J0X2ltYWdlOnRlc3QiXX0%3D&_nc_ohc=qmWISgwWNh4Q7kNvwFVQ6N_&_nc_oc=AdlNWneZykDImYYMnykQpWpodj_cKyLE0f4G-sJqSWFdLuedungpqw9rxJlPoMIJ6nk&_nc_zt=23&_nc_ht=instagram.fblr20-2.fna&oh=03_Q7cD4gGNjK_fVDEG0Jdx5E70B9K0AM1VsQ3pEYN5uwBab8pRAA&oe=69AEB84E"  
+css = f"""
+<style>
+  .stApp {{
+    background-image: url("{bg_url}");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+
+  }}
+  .bg-overlay {{
+    position: fixed; inset: 0; pointer-events: none; z-index: 0;
+    background: rgba(0,5,25,0.75);
+  }}
+  .main > div {{ position: relative; z-index: 1; }}
+</style>
+<div class="bg-overlay"></div>
+"""
+st.markdown(css, unsafe_allow_html=True)
+
+st.subheader("Results")
+
+df = pd.DataFrame(
+    {
+        "Problem": ["problem 1", "problem 2", "problem 3"],
+        "Description": ["explanation 1", "explanation 2", "explanation 3"],
+        "Priority": [5, 3, 1],
+    }
+)
+
+st.dataframe(
+    df,
+    column_config={
+        "Problem": st.column_config.Column(width="small"),
+        "Description": st.column_config.Column(width="large"),
+        "Priority": st.column_config.Column(width="small"),
+    },
+    use_container_width=True,
+    hide_index=True,
+)
+
+
+st.write("Overall Rating:")
+st.progress(67, text="67/100")
