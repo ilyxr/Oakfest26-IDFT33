32f0c212c623b017992f3469f9d4813a139c1022
mr beats <hackieuser@gmail.com>
2026-02-07 13:03:23 +0530

    Added initial WIP frontend

--- EarlyWIP/epsteinFile.py
+++ EarlyWIP/epsteinFile.py
@@ -1,23 +1,39 @@
-from github import Github
+import streamlit as st
+import pandas as pd 
 
-# Authentication is defined via github.Auth
-from github import Auth
+st.title("name of the project (i forgor)")
 
-# using an access token
-
-key = Auth.Token(input("Enter your GitHub personal access token: "))
-data = {} 
-
-def getGitRepo(auth, repo_name): 
-    i=0
-    g = Github(auth=auth) 
-    repo = g.get_repo(repo_name)
-    contents = repo.get_contents("") 
-    while contents: 
-        file_content = contents.pop(0)
-        if file_content.type == "dir":
-            contents.extend(repo.get_contents(file_content.path))
-        else:
-            data[file_content.path] = repo.get_contents(str(file_content.path)).decoded_content
-
-    g.close()
+def page1(): 
+    st.write("Please enter your Github File's link")
+    github_link = st.text_input("Github File Link: ", key="github_link") 
+    if st.button("Submit", key="submit_button"):
+        st.write("You entered: ", github_link)
+        # the function would be called right HERE 
+        # st.spinner() 
+        # st.success("Analysis complete!") 
+        # st.button("Go to Results", key="go_to_results_button")
+        # if st.button("Go to Results", key="go_to_results_button"):
+        #   st.switch_page("Page2")
+        
+def page2(): 
+    st.write("Here are the results of the analysis: ")
+    col1, col2 = st.columns(2) 
+    
+    with col1: 
+        st.write(
+            pd.DataFrame(
+                {
+                    "Problem": ["problem 1", "problem 2", "problem 3"],
+                    "Description": ["explanation 1", "explanation 2", "explanation 3"],
+                }
+            ))
+        
+    with col2: 
+        st.write("Overall Rating:")
+        st.progress(80, text="4/5")
+    
+pg = st.navigation([
+    st.Page(page1, title="Enter Link"),
+    st.Page(page2, title="Results") 
+], position="hidden")
+pg.run()
