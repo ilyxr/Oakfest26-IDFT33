2b832e410bceceaf9955c77de14278f895176947
mr beats <hackieuser@gmail.com>
2026-02-07 11:40:03 +0530

    Fixed a bug regarding the repository name in the getGitRepo function.

--- EarlyWIP/epsteinFile.py
+++ EarlyWIP/epsteinFile.py
@@ -6,16 +6,18 @@ from github import Auth
 # using an access token
 
 key = Auth.Token(input("Enter your GitHub personal access token: "))
+data = {} 
 
-def getGitRepo(auth, repo): 
+def getGitRepo(auth, repo_name): 
+    i=0
     g = Github(auth=auth) 
-    repo = g.get_repo(auth=auth)
-    contents = repo.get_contents() 
-    while contents:
+    repo = g.get_repo(repo_name)
+    contents = repo.get_contents("") 
+    while contents: 
         file_content = contents.pop(0)
         if file_content.type == "dir":
             contents.extend(repo.get_contents(file_content.path))
         else:
-            print(file_content) 
+            data[file_content.path] = repo.get_contents(str(file_content.path)).decoded_content
 
     g.close()
