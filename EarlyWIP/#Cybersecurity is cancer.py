e8b10f9f1e41c0281a4aec7432a2fc474314ab1f
quick no. 2 <157027960+ilyxr@users.noreply.github.com>
2026-02-06 22:36:20 -0800

    Github API link to read files

--- EarlyWIP/#Cybersecurity is cancer.py
+++ EarlyWIP/#Cybersecurity is cancer.py
@@ -1,33 +1,48 @@
-#Cybersecurity is cancer
-
-from google import genai
-from google.genai import types
-import httpx
-import os
+import requests
 
 from dotenv import load_dotenv
 
 load_dotenv()
+###edge
 
+def github_read_file(username, repository_name, file_path, github_token=None):
+    headers = {}
+    if github_token:
+        headers['Authorization'] = f"token {github_token}"
+        
+    url = f'https://api.github.com/repos/{username}/{repository_name}/contents/{file_path}'
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()
+    data = r.json()
+    file_content = data['content']
+    file_content_encoding = data.get('encoding')
+    if file_content_encoding == 'base64':
+        file_content = base64.b64decode(file_content).decode()
+
+    return file_content
+
+
+def main():
+    github_token = os.environ['GITHUB_TOKEN']
+    username = 'airbnb'
+    repository_name = 'javascript'
+    file_path = 'package.json'
+    file_content = github_read_file(username, repository_name, file_path, github_token=github_token)
+    data = json.loads(file_content)
+    print(data)
+
+if __name__ == '__main__':
+    main()
+
+###goon
 api_key1 = os.getenv("GOOGLE_API_KEY")
 
 client = genai.Client(api_key=api_key1)
-
-#doc_url = str(input("Enter: "))
-doc_url = "https://discovery.ucl.ac.uk/id/eprint/10089234/1/343019_3_art_0_py4t4l_convrt.pdf"
-
-doc_data = httpx.get(doc_url).content
-
-prompt = "Summarize this document"
+'''
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
-    contents=[
-        types.Part.from_bytes(
-            data=doc_data,
-            mime_type='application/pdf',
-        ),
-        prompt
-    ]
+    contents="How does AI work?"
 )
 
 print(response.text)
+'''
